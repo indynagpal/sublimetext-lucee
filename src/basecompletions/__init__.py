2ad303f17b611ea87058a2702677984bd19f8ca6
jcberquist <jcberquist@outlook.com>
2015-10-20 21:32:00 -0400

    added CGI scope completions and inline documentation (where available)

--- src/basecompletions/__init__.py
+++ src/basecompletions/__init__.py
@@ -1,8 +1,9 @@
-from .. import completions
+from .. import completions, inline_documentation
 from . import basecompletions
 
 basecompletions.load_completions()
 completions.add_completion_source('tag', basecompletions.get_tags)
 completions.add_completion_source('tag_attributes', basecompletions.get_tag_attributes)
 completions.add_completion_source('script', basecompletions.get_script_completions)
-completions.add_completion_source('dot', basecompletions.get_member_functions)
+completions.add_completion_source('dot', basecompletions.get_dot_completions)
+inline_documentation.add_documentation_source(basecompletions.get_inline_documentation)
--- src/basecompletions/basecompletions.py
+++ src/basecompletions/basecompletions.py
@@ -1,10 +1,20 @@
 import json
 from os.path import dirname, realpath
 from ..completions import CompletionList
+from ..inline_documentation import Documentation
+from .. import utils
 
 FULL_PATH = dirname(realpath(__file__)).replace("\\", "/")
 COMPLETION_FILES = ["lucee_tags","lucee_functions","lucee_member_functions","cfml_tags","cfml_functions","cfml_member_functions"]
+DOC_STYLES = {
+	"side_color": "#4C9BB0",
+	"header_color": "#306B7B",
+	"header_bg_color": "#E4EEF1",
+	"text_color": "#272B33"
+}
+
 completions = {}
+cgi = {}
 
 def get_tags(view, prefix, position, info):
 	completion_list = completions[info["dialect"] + "_tags"]
@@ -26,12 +36,30 @@ def get_script_completions(view, prefix, position, info):
 	completion_list.extend(completions[info["dialect"] + "_tags_in_script"])
 	return CompletionList(completion_list, 0, False)
 
-def get_member_functions(view, prefix, position, info):
+def get_dot_completions(view, prefix, position, info):
+
+	if len(info["dot_context"]) == 1 and info["dot_context"][0].name == "cgi":
+		return CompletionList(completions["cgi"], 1, True)
+
 	completion_list = completions[info["dialect"] + "_member_functions"]
 	return CompletionList(completion_list, 0, False)
 
+def get_inline_documentation(view, position):
+
+	if view.match_selector(position, "variable.other.constant"):
+		word = view.word(position)
+		dot_context = utils.get_dot_context(view, word.begin() - 1)
+		if len(dot_context) == 1 and dot_context[0].name == "cgi":
+			key = "cgi." + view.substr(word).lower()
+			if key in cgi:
+				doc = dict(DOC_STYLES)
+				doc.update(cgi[key])
+				return Documentation(doc, None, 1)
+
+	return None
+
 def load_completions():
-	global completions
+	global completions, cgi
 	completions_data = {filename: load_json_data(filename) for filename in COMPLETION_FILES}
 
 	for dialect in ["lucee","cfml"]:
@@ -55,6 +83,10 @@ def load_completions():
 			for funct in sorted(completions_data[dialect + "_member_functions"][member_function_type].keys()):
 				mem_func_comp.append( (funct + '\t' + member_function_type + '.fn (' + dialect + ')', funct + completions_data[dialect + "_member_functions"][member_function_type][funct]))
 		completions[dialect + "_member_functions"] = mem_func_comp
+		
+		# CGI scope
+		cgi = load_json_data("cgi")
+		completions["cgi"] = [(scope_variable.split(".").pop().upper() + "\tCGI", scope_variable.split(".").pop().upper()) for scope_variable in sorted(cgi.keys())]
 
 def load_json_data(filename):
 	with open(FULL_PATH + '/json/' + filename + '.json', 'r') as f:
